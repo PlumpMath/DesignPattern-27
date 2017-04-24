3ad58a83586f78f97e97ca21501ba5eebc9c5f21
czqlan <ziqiang.chen@outlook.com>
2017-04-24 21:31:36 +0800

    Delete All

--- src/com/chenziqiang/_2_FactoryPattern/CreateHuman/AbstractHumanFactory.java
+++
@@ -1,8 +0,0 @@
-package com.chenziqiang._2_FactoryPattern.CreateHuman;
-
-/**
- * Created by chenziqiang on 2016/12/17.
- */
-public abstract class AbstractHumanFactory {
-    public abstract <T extends Human> T createHuman(Class<T> c);
-}
--- src/com/chenziqiang/_2_FactoryPattern/CreateHuman/Human.java
+++
@@ -1,9 +0,0 @@
-package com.chenziqiang._2_FactoryPattern.CreateHuman;
-
-/**
- * Created by chenziqiang on 2016/12/17.
- */
-public interface Human {
-    public void getColor();
-    public void talk();
-}
